00c045def612f89c78454fbfa5b7dcbfc435be08
Vlad Hryh <vgrugor@gmail.com>
2023-12-25 17:36:45 +0200

    add http request

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 #include "App/Buzzer/Buzzer.h"
 #include "App/MotionSensor/MotionSensor.h"
 
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
 WiFiConnectionFacade WiFiConnection {WIFI_SSID, WIFI_PASSWORD, WIFI_IP, WIFI_GATEWAY, WIFI_SUBNET};
 Buzzer BuzzerSignalizator {BUZZER_PIN};
 MotionSensor Sensor {MOTION_SENSOR_PIN};
@@ -30,6 +34,29 @@ void loop() {
 	}
 
 	if (Sensor.hasMotionDetected()) {
+		WiFiClient client;
+		HTTPClient http;
+
+		String serverPath = "http://192.168.1.200:80/sensor/motion?sensor=1";
+		
+		http.begin(client, serverPath.c_str());
+
+		int httpResponseCode = http.GET();
+		
+		if (httpResponseCode > 0) {
+			Serial.print("HTTP Response code: ");
+			Serial.println(httpResponseCode);
+			String payload = http.getString();
+			Serial.println(payload);
+		}
+		else {
+			Serial.print("Error code: ");
+			Serial.println(httpResponseCode);
+		}
+
+		http.end();
+
+
 		BuzzerSignalizator.motionDetected();
 	}
 }
